4514531b9b4dc7413d7583d2f3d6eb998f1d300b
Kayode Olumoyin <kayodaniel01@gmail.com>
2024-01-19 13:40:58 -0500

    Delete build directory

--- build/lib/pyLinG3D/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import utils
-
-from .linG3DAliveAll import linG3DAliveAll
-from .linG3DAliveClone import linG3DAliveClone
-from .linG3DAll import linG3DAll
-from .linG3DClone import linG3DClone
